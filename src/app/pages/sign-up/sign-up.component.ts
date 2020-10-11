0d0e7d718db8b5bf18cd41f442c139145f65d99c
Irakli Dzandzava <iraklidzandzava1@gmail.com>
2020-10-11 13:11:44 +0400

    finished sign-up markup and some logic

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -6,6 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  public currentDate = new Date();
+  public currentDay: number = this.currentDate.getDate();
+  public currentMonth: number = this.currentDate.getMonth() + 1;
+  public currentYear: number = this.currentDate.getFullYear();
+  public days: number[] = Array(31)
+    .fill(null)
+    .map((_, i) => i + 1);
+  public months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  public startYear = 1900;
+  public years: number[] = Array(this.currentYear - this.startYear + 1)
+    .fill(null)
+    .map((_, i) => this.currentYear - i);
 
   constructor() { }
 
